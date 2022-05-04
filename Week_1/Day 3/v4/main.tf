76170527772af0319dec6e50abec43c110977b76
Keith Williams <52156280+keithdw1972@users.noreply.github.com>
2022-05-04 15:36:47 +0100

    15:35

--- Week_1/Day 3/v4/main.tf
+++ Week_1/Day 3/v4/main.tf
@@ -1,19 +1,11 @@
 terraform {
-
     required_providers {
-
-        azurerm = {
-
-            source =  "hashicorp/azurerm"
-
-                version = ">=2.29.0"
-
-}
-
-}
-
-required_version = "~>1.1.6"
-
+      azurerm = {
+        source =  "hashicorp/azurerm"
+        version = ">=2.29.0"
+      }
+ }
+  required_version = "~>1.1.6"
 }
 provider "azurerm" {
   features {}
